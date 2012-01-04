0e228d84613e26575d9ad0b665e9951d76c868f2
Rusty Russell <rusty@rustcorp.com.au>
2012-01-04 14:36:21 +1030

    ls: forward port to modern Unix.
    Note that this compiles, but crashes due to assumption that it can
    just use sbrk.

--- ls/ls-ported-forward.c
+++ ls/ls-ported-forward.c
@@ -1,14 +1,19 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <dirent.h>
+
 /*
  * list file or directory
  */
 
-struct {
+struct buffered_file {
 	int	fdes;
 	int	nleft;
 	char	*nextc;
 	char	buff[512];
 } inf;
 
+#if 0
 struct ibuf {
 	int	idev;
 	int	inum;
@@ -19,20 +24,21 @@ struct ibuf {
 	char	isize0;
 	int	isize;
 	int	iaddr[8];
-	char	*iatime[2];
-	char	*imtime[2];
+	time_t	iatime;
+	time_t	imtime;
 };
+#endif
 
 struct lbuf {
-	char	lname[15];
+	char	lname[257];
 	int	lnum;
 	int	lflags;
-	char	lnl;
-	char	luid;
-	char	lgid;
+	int	lnl;
+	int	luid;
+	int	lgid;
 	char	lsize0;
 	int	lsize;
-	char	*lmtime[2];
+	time_t	lmtime;
 };
 
 struct lbufx {
@@ -41,20 +47,73 @@ struct lbufx {
 
 int	aflg, dflg, lflg, sflg, tflg, uflg, iflg, fflg, gflg;
 int	fout;
-int	rflg	1;
-char	*year;
+int	rflg = 1;
+unsigned year;
 int	flags;
-int	uidfil	-1;
-int	lastuid	-1;
+int	uidfil = -1;
+int	lastuid	= -1;
 char	tbuf[16];
 int	tblocks;
 int	statreq;
-struct	lbuf	*lastp	&end;
-struct	lbuf	*rlastp	&end;
-char	*dotp	".";
+struct	lbuf	*lastp;
+struct	lbuf	*rlastp;
+char	*dotp = ".";
+
+int printf(const char *format, ...);
+extern void *stdout;
+int fflush(void *stream);
+char *ctime(const time_t *timep);
+void *sbrk(long increment);
+
+/* part of V6 standard library. */
+static char *locv(int hi, int lo)
+{
+	static char buffer[32];
+	int sprintf(char *, const char *format, ...);
+	sprintf(buffer, "%u", (hi << 16) | lo);
+	return buffer;
+}
+
+static int ldiv(int hi, int lo, int div)
+{
+	return ((hi << 16) | lo) / div;
+}
+
+static void seek(int fd, int off, int whence)
+{
+	lseek(fd, 0, 0);
+}
+
+/* This is in the standard library for V6, in asm! */
+static int fopen_(char *file, struct buffered_file *bf)
+{
+	bf->fdes = open(file, O_RDWR);
+	bf->nleft = 0;
+	bf->nextc = bf->buff;
+	return bf->fdes;
+}
+
+static int getc_(struct buffered_file *bf)
+{
+	if (bf->nleft) {
+		bf->nleft--;
+		return *(bf->nextc++);
+	}
+	bf->nextc = bf->buff;
+	bf->nleft = read(bf->fdes, &bf->buff, sizeof(bf->buff));
+	if (bf->nleft > 0) {
+		bf->nleft--;
+		return *(bf->nextc++);
+	}
+	return -1;
+}
+
+/* pre-declare */
+struct lbuf *gstat();
+int select_(int *pairp);
 
 #define	IFMT	060000
-#define	DIR	0100000
+#define	DIRX	0100000
 #define	CHR	020000
 #define	BLK	040000
 #define	ISARG	01000
@@ -77,16 +136,16 @@ main(argc, argv)
 char **argv;
 {
 	int i, j;
-	extern struct lbuf end;
 	register struct lbuf *ep, *ep1;
-	register struct lbuf *slastp;
+	register struct lbuf *slastp, *firstp;
 	struct lbuf lb;
 	int t;
 	int compar();
 
+	firstp = rlastp = lastp = sbrk(0);
 	fout = dup(1);
-	time(lb.lmtime);
-	year = lb.lmtime[0] - 245; /* 6 months ago */
+	time(&lb.lmtime);
+	year = (lb.lmtime >> 16) - 245; /* 6 months ago */
 	if (--argc > 0 && *argv[1] == '-') {
 		argv++;
 		while (*++*argv) switch (**argv) {
@@ -146,7 +205,7 @@ char **argv;
 		statreq = 0;
 	}
 	if(lflg) {
-		t = "/etc/passwd";
+		char *t = "/etc/passwd";
 		if(gflg)
 			t = "/etc/group";
 		uidfil = open(t, 0);
@@ -158,17 +217,17 @@ char **argv;
 	for (i=0; i < argc; i++) {
 		if ((ep = gstat(*++argv, 1))==0)
 			continue;
-		ep->namep = *argv;
-		ep->lflags =| ISARG;
+		((struct lbufx *)ep)->namep = *argv;
+		ep->lflags |= ISARG;
 	}
-	qsort(&end, lastp - &end, sizeof *lastp, compar);
+	qsort(firstp, lastp - firstp, sizeof *lastp, compar);
 	slastp = lastp;
-	for (ep = &end; ep<slastp; ep++) {
-		if (ep->lflags&DIR && dflg==0 || fflg) {
+	for (ep = firstp; ep<slastp; ep++) {
+		if (ep->lflags&DIRX && dflg==0 || fflg) {
 			if (argc>1)
-				printf("\n%s:\n", ep->namep);
+				printf("\n%s:\n", ((struct lbufx *)ep)->namep);
 			lastp = slastp;
-			readdir(ep->namep);
+			readdir_(((struct lbufx *)ep)->namep);
 			if (fflg==0)
 				qsort(slastp,lastp - slastp,sizeof *lastp,compar);
 			if (statreq)
@@ -178,13 +237,13 @@ char **argv;
 		} else 
 			pentry(ep);
 	}
-	flush();
+	fflush(stdout);
+	return 0;
 }
 
 pentry(ap)
 struct lbuf *ap;
 {
-	struct { char dminor, dmajor;};
 	register t;
 	register struct lbuf *p;
 	register char *cp;
@@ -200,26 +259,29 @@ struct lbuf *ap;
 		t = p->luid;
 		if(gflg)
 			t = p->lgid;
-		t =& 0377;
+#if 0
+		t &= 0377;
+#endif
 		if (getname(t, tbuf)==0)
 			printf("%-6.6s", tbuf);
 		else
 			printf("%-6d", t);
-		if (p->lflags & (BLK|CHR))
-			printf("%3d,%3d", p->lsize.dmajor&0377,
-			    p->lsize.dminor&0377);
-		else
+		if (p->lflags & (BLK|CHR)) {
+			struct { char dminor, dmajor;} *dev = (void *)&p->lsize;
+			printf("%3d,%3d", dev->dmajor&0377,
+			    dev->dminor&0377);
+		} else
 			printf("%7s", locv(p->lsize0, p->lsize));
-		cp = ctime(p->lmtime);
-		if(p->lmtime[0] < year)
+		cp = ctime(&p->lmtime);
+		if((p->lmtime >> 16) < year)
 			printf(" %-7.7s %-4.4s ", cp+4, cp+20); else
 			printf(" %-12.12s ", cp+4);
 	} else if (sflg)
 		printf("%4d ", nblock(p->lsize0, p->lsize));
 	if (p->lflags&ISARG)
-		printf("%s\n", p->namep);
+		printf("%s\n", ((struct lbufx *)p)->namep);
 	else
-		printf("%.14s\n", p->lname);
+		printf("%-14s\n", p->lname);
 }
 
 getname(uid, buf)
@@ -238,7 +300,7 @@ char buf[];
 		i = 0;
 		j = 0;
 		n = 0;
-		while((c=getc(&inf)) != '\n') {
+		while((c=getc_(&inf)) != '\n') {
 			if (c<0)
 				return(-1);
 			if (c==':') {
@@ -257,7 +319,7 @@ char buf[];
 }
 
 nblock(size0, size)
-char *size0, *size;
+unsigned size0, size;
 {
 	register int n;
 
@@ -269,19 +331,19 @@ char *size0, *size;
 	return(n);
 }
 
-int	m0[] { 3, DIR, 'd', BLK, 'b', CHR, 'c', '-'};
-int	m1[] { 1, ROWN, 'r', '-' };
-int	m2[] { 1, WOWN, 'w', '-' };
-int	m3[] { 2, SUID, 's', XOWN, 'x', '-' };
-int	m4[] { 1, RGRP, 'r', '-' };
-int	m5[] { 1, WGRP, 'w', '-' };
-int	m6[] { 2, SGID, 's', XGRP, 'x', '-' };
-int	m7[] { 1, ROTH, 'r', '-' };
-int	m8[] { 1, WOTH, 'w', '-' };
-int	m9[] { 1, XOTH, 'x', '-' };
-int	m10[] { 1, STXT, 't', ' ' };
+int	m0[] = { 3, DIRX, 'd', BLK, 'b', CHR, 'c', '-'};
+int	m1[] = { 1, ROWN, 'r', '-' };
+int	m2[] = { 1, WOWN, 'w', '-' };
+int	m3[] = { 2, SUID, 's', XOWN, 'x', '-' };
+int	m4[] = { 1, RGRP, 'r', '-' };
+int	m5[] = { 1, WGRP, 'w', '-' };
+int	m6[] = { 2, SGID, 's', XGRP, 'x', '-' };
+int	m7[] = { 1, ROTH, 'r', '-' };
+int	m8[] = { 1, WOTH, 'w', '-' };
+int	m9[] = { 1, XOTH, 'x', '-' };
+int	m10[] = { 1, STXT, 't', ' ' };
 
-int	*m[] { m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10};
+int	*m[] = { m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10};
 
 pmode(aflag)
 {
@@ -289,11 +351,10 @@ pmode(aflag)
 
 	flags = aflag;
 	for (mp = &m[0]; mp < &m[11];)
-		select(*mp++);
+		select_(*mp++);
 }
 
-select(pairp)
-int *pairp;
+int select_(int *pairp)
 {
 	register int n, *ap;
 
@@ -304,10 +365,10 @@ int *pairp;
 	putchar(*ap);
 }
 
-makename(dir, file)
+char *makename(dir, file)
 char *dir, *file;
 {
-	static char dfile[100];
+	static char dfile[1000];
 	register char *dp, *fp;
 	register int i;
 
@@ -317,15 +378,16 @@ char *dir, *file;
 		*dp++ = *fp++;
 	*dp++ = '/';
 	fp = file;
-	for (i=0; i<14; i++)
+	for (i=0; i<256; i++)
 		*dp++ = *fp++;
 	*dp = 0;
 	return(dfile);
 }
 
-readdir(dir)
+readdir_(dir)
 char *dir;
 {
+#if 0
 	static struct {
 		int	dinode;
 		char	dname[14];
@@ -355,17 +417,42 @@ char *dir;
 			ep->lname[j] = dentry.dname[j];
 	}
 	close(inf.fdes);
+#else
+	struct dirent *dirp;
+	register struct lbuf *ep;
+	register int j;
+	DIR *d;
+
+	d = opendir(dir);
+	if (!d) {
+		printf("%s unreadable\n", dir);
+		return;
+	}
+	tblocks = 0;
+	for(;;) {
+		dirp = readdir(d);
+		if (!dirp)
+			break;
+		if (dirp->d_ino == 0
+		 || aflg==0 && dirp->d_name[0]=='.')
+			continue;
+		ep = gstat(makename(dir, dirp->d_name), 0);
+		if (ep->lnum != -1)
+			ep->lnum = dirp->d_ino;
+		for (j=0; j<256; j++)
+			ep->lname[j] = dirp->d_name[j];
+	}
+#endif	
 }
 
-gstat(file, argfl)
+struct lbuf *gstat(file, argfl)
 char *file;
 {
-	struct ibuf statb;
+	struct stat statb;
 	register struct lbuf *rep;
 
 	if (lastp+1 >= rlastp) {
-		sbrk(512);
-		rlastp.idev =+ 512;
+		rlastp = sbrk(512) + 512;
 	}
 	rep = lastp;
 	lastp++;
@@ -374,42 +461,39 @@ char *file;
 	if (argfl || statreq) {
 		if (stat(file, &statb)<0) {
 			printf("%s not found\n", file);
-			statb.inum = -1;
-			statb.isize0 = 0;
-			statb.isize = 0;
-			statb.iflags = 0;
+			statb.st_ino = -1;
+			statb.st_size = 0;
+			statb.st_mode = 0;
 			if (argfl) {
 				lastp--;
 				return(0);
 			}
 		}
-		rep->lnum = statb.inum;
-		statb.iflags =& ~DIR;
-		if ((statb.iflags&IFMT) == 060000) {
-			statb.iflags =& ~020000;
-		} else if ((statb.iflags&IFMT)==040000) {
-			statb.iflags =& ~IFMT;
-			statb.iflags =| DIR;
+		rep->lnum = statb.st_ino;
+		statb.st_mode &= ~DIRX;
+		if ((statb.st_mode&IFMT) == 060000) {
+			statb.st_mode &= ~020000;
+		} else if ((statb.st_mode&IFMT)==040000) {
+			statb.st_mode &= ~IFMT;
+			statb.st_mode |= DIRX;
 		}
-		statb.iflags =& ~ LARGE;
-		if (statb.iflags & RSTXT)
-			statb.iflags =| STXT;
-		statb.iflags =& ~ RSTXT;
-		rep->lflags = statb.iflags;
-		rep->luid = statb.iuid;
-		rep->lgid = statb.igid;
-		rep->lnl = statb.inl;
-		rep->lsize0 = statb.isize0;
-		rep->lsize = statb.isize;
+		statb.st_mode &= ~ LARGE;
+		if (statb.st_mode & RSTXT)
+			statb.st_mode |= STXT;
+		statb.st_mode &= ~ RSTXT;
+		rep->lflags = statb.st_mode;
+		rep->luid = statb.st_uid;
+		rep->lgid = statb.st_gid;
+		rep->lnl = statb.st_nlink;
+		rep->lsize0 = statb.st_size >> 16;
+		rep->lsize = statb.st_size & 0xFFFF;
 		if (rep->lflags & (BLK|CHR) && lflg)
-			rep->lsize = statb.iaddr[0];
-		rep->lmtime[0] = statb.imtime[0];
-		rep->lmtime[1] = statb.imtime[1];
+			rep->lsize = statb.st_rdev;
+		rep->lmtime = statb.st_mtime;
 		if(uflg) {
-			rep->lmtime[0] = statb.iatime[0];
-			rep->lmtime[1] = statb.iatime[1];
+			rep->lmtime = statb.st_atime;
 		}
-		tblocks =+ nblock(statb.isize0, statb.isize);
+		tblocks =+ nblock(0, statb.st_size);
 	}
 	return(rep);
 }
@@ -420,41 +504,37 @@ struct lbuf *ap1, *ap2;
 	register struct lbuf *p1, *p2;
 	register int i;
 	int j;
-	struct { char *charp;};
+	char *p1c, *p2c;
 
 	p1 = ap1;
 	p2 = ap2;
 	if (dflg==0) {
-		if ((p1->lflags&(DIR|ISARG)) == (DIR|ISARG)) {
-			if ((p2->lflags&(DIR|ISARG)) != (DIR|ISARG))
+		if ((p1->lflags&(DIRX|ISARG)) == (DIRX|ISARG)) {
+			if ((p2->lflags&(DIRX|ISARG)) != (DIRX|ISARG))
 				return(1);
 		} else {
-			if ((p2->lflags&(DIR|ISARG)) == (DIR|ISARG))
+			if ((p2->lflags&(DIRX|ISARG)) == (DIRX|ISARG))
 				return(-1);
 		}
 	}
 	if (tflg) {
 		i = 0;
-		if (p2->lmtime[0] > p1->lmtime[0])
+		if (p2->lmtime > p1->lmtime)
 			i++;
-		else if (p2->lmtime[0] < p1->lmtime[0])
-			i--;
-		else if (p2->lmtime[1] > p1->lmtime[1])
-			i++;
-		else if (p2->lmtime[1] < p1->lmtime[1])
+		else if (p2->lmtime < p1->lmtime)
 			i--;
 		return(i*rflg);
 	}
 	if (p1->lflags&ISARG)
-		p1 = p1->namep;
+		p1c = ((struct lbufx *)p1)->namep;
 	else
-		p1 = p1->lname;
+		p1c = p1->lname;
 	if (p2->lflags&ISARG)
-		p2 = p2->namep;
+		p2c = ((struct lbufx *)p2)->namep;
 	else
-		p2 = p2->lname;
+		p2c = p2->lname;
 	for (;;)
-		if ((j = *p1.charp++ - *p2.charp++) || p1.charp[-1]==0)
+		if ((j = *p1c++ - *p2c++) || p1c[-1]==0)
 			return(rflg*j);
 	return(0);
 }
