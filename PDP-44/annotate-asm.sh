d6c1e04212caccea157704026ba494135fdafd27
Rusty Russell <rusty@rustcorp.com.au>
2012-01-11 12:14:58 +1030

    Don't assume an extra byte. We could play with encodings, and also not all jumps will need to be big, etc.

--- PDP-44/annotate-asm.sh
+++ PDP-44/annotate-asm.sh
@@ -5,22 +5,21 @@
 # Any register offset => assume 24 bit relative (+3 bytes)
 # Any branch => Assume 48 bit offset address (+6 bytes)
 # Any literal => Assume 32 bit value (+4 bytes)
-# And any modified instruction, add 1 byte prefix.
 
 ADDITIONAL=0
 while read LINE; do
     case "$LINE" in
 	*[0-9]\(*)
 	    echo "$LINE		# 3"
-	    ADDITIONAL=$(($ADDITIONAL + 4))
+	    ADDITIONAL=$(($ADDITIONAL + 3))
 	    ;;
 	*br*|*bhi*|*beq*|*bne*|*bge*|*jsr*|*jmp*)
 	    echo "$LINE		# 6"
-	    ADDITIONAL=$(($ADDITIONAL + 7))
+	    ADDITIONAL=$(($ADDITIONAL + 6))
 	    ;;
 	*\$*)
 	    echo "$LINE		# 4"
-	    ADDITIONAL=$(($ADDITIONAL + 5))
+	    ADDITIONAL=$(($ADDITIONAL + 4))
 	    ;;
 	*)
 	    echo "$LINE"
